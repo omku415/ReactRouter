29a9b0a0b2d4de95eba0e127ef38dba66cae22e5
Om Kumar <omku415@gmail.com>
2025-03-15 13:38:04 +0530

    Updated files and added new components

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,27 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import Layout from "./Layout";
+import Home from "./component/home/Home";
+import About from "./component/about/About";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
